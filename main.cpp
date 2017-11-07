1b52b3ac8b699919d8bb9bd93523fa39cb430aee
kbaedal <kbaedal@gmail.com>
2017-11-07 18:21:13 +0100

    Funciona, el muy bastardo.

--- main.cpp
+++ main.cpp
@@ -41,18 +41,42 @@ vec3 color(const ray &r, hitable *world, int depth)
     }
 }
 
-int main()
+hitable *random_scene()
 {
-    int nx = 4*200;
-    int ny = 4*100;
-    int ns = 100;
-        
-    std::ofstream myfile("test.ppm");
-
-    myfile << "P3\n" << nx << " " << ny << "\n255\n";
+    int n = 500;
+    hitable **list = new hitable*[n+1];
+    list[0] = new sphere(vec3(0.0, -1000.0, -1.0), 1000.0, new lambertian(vec3(0.5, 0.5, 0.5)));
     
+    int i = 1;
+    
+    for(int a = -11; a < 11; ++a) {
+        for(int b = -11; b < 11; ++b) {
+            float choose_mat = dis(gen);
+            vec3 center(a+0.9*dis(gen), 0.2, b+0.9*dis(gen));
+            if((center-vec3(4.0, 0.2, 0.0)).length() > 0.9) {
+                if( choose_mat < 0.8 ) { // diffuse
+                    list[i++] = new sphere(center, 0.2, new lambertian(vec3(dis(gen)*dis(gen), dis(gen)*dis(gen), dis(gen)*dis(gen))));
+                }
+                else if( choose_mat < 0.95 ) { // metal
+                    list[i++] = new sphere(center, 0.2, new metal(vec3(0.5 * (1 + dis(gen)), 0.5 * (1 + dis(gen)), 0.5 * (1 + dis(gen))), 0.5 * dis(gen)));
+                }
+                else { // glass
+                    list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+                }
+            }
+        }
+    }
 
-    hitable *list[5];
+    list[i++] = new sphere(vec3(0.0, 1.0, 0.0), 1.0, new dielectric(1.5));
+    list[i++] = new sphere(vec3(-4.0, 1.0, 0.0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+    list[i++] = new sphere(vec3(4.0, 1.0, 0.0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+    
+    return new hitable_list(list, i);
+}
+
+hitable *standard_scene()
+{
+    hitable **list = new hitable*[5];
     
     list[0] = new sphere(vec3(0.0, 0.0, -1.0), 0.5, new lambertian(vec3(0.3, 0.3, 0.8)));    
     list[1] = new sphere(vec3(0.0, -100.5, -1.0), 100.0, new lambertian(vec3(0.8, 0.8, 0.0)));
@@ -60,9 +84,37 @@ int main()
     list[3] = new sphere(vec3(-1.0, 0.0, -1.0), 0.5, new dielectric(1.5));
     list[4] = new sphere(vec3(-1.0, 0.0, -1.0), -0.45, new dielectric(1.5));
    
-    hitable *world = new hitable_list(list, 5);
+    return new hitable_list(list, 5);
+}
+
+int main()
+{
+    int nx = 6*200;
+    int ny = 6*100;
+    int ns = 100;
+        
+    std::ofstream myfile("test.ppm");
+
+    myfile << "P3\n" << nx << " " << ny << "\n255\n";
     
-    camera cam(vec3(-2.0, 2.0, 1.0), vec3(0.0, 0.0, -1.0), vec3(0.0, 1.0, 0.0), 20, float(nx)/float(ny));
+    //hitable *world = standard_scene();
+    hitable *world = random_scene();
+    
+    /*
+    vec3 lookfrom(3.0, 3.0, 2.0);
+    vec3 lookat(0.0, 0.0, -1.0);
+    vec3 camup(0.0, 1.0, 0.0);
+    float dist_to_focus = (lookfrom - lookat).length();
+    float aperture = 2.0;
+    */
+    
+    vec3 lookfrom(13.0, 2.0, 3.0);
+    vec3 lookat(0.0, 0.0, 0.0);
+    vec3 camup(0.0, 1.0, 0.0);
+    float dist_to_focus = 10.0;
+    float aperture = 0.1;
+    
+    camera cam(lookfrom, lookat, camup, 20, float(nx)/float(ny), aperture, dist_to_focus);
 
     for(int j = ny - 1; j >= 0; --j) {
         for(int i = 0; i < nx; ++i) {
